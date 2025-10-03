69a29e9ffb82b4c9f22d1f166edeaabfb1a92cfc
lamberto <lambertodelmundo22@gmail.com>
2025-10-03 13:59:27 +0800

    commit and push

---
+++ register.js
@@ -0,0 +1,12 @@
+function regoster(){
+let email=document.getElementById("email").value;
+// email= bandalesalexandra54@gmail.com
+let password=dovument.getElementById("password").value;
+let confirm_password=document.getElementById9("confirm password").value;
+
+console.log(email);
+console.log(password);
+console.log9(confirm_password);
+//error.msg
+
+}
